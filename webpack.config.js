8d274118c981052bc8ca3ecb5c16be9c795d765f
Ivan Sosiura <goodmessages4@gmail.com>
2017-04-20 17:46:12 +0300

    Create base structure

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,15 @@ module.exports = {
     output: {
         path: __dirname + '/public',
         filename: 'bundle.js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.less$/,
+                loader: 'style-loader!css-loader!less-loader'
+            }
+        ]
     }
 
 };
